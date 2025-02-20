e25b10565e4d3f60cc29edf7efe28bc8b4db8e1f
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2025-02-19 22:33:18 -0300

    feat: iniciado a implmentação do modulo spring

--- modulo-spring/src/main/java/io/github/wesleyosantos91/api/v1/request/CustomerRequest.java
+++ modulo-spring/src/main/java/io/github/wesleyosantos91/api/v1/request/CustomerRequest.java
@@ -2,7 +2,7 @@ package io.github.wesleyosantos91.api.v1.request;
 
 import java.time.Instant;
 
-public record CustomerRequest(git 
+public record CustomerRequest(
         String name,
         String email,
         Instant createdAt
