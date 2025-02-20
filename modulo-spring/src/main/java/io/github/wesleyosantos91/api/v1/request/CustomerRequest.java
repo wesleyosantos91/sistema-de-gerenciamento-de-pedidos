cb6c7aece3270e8461543b299450837aa7ebed24
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2025-02-19 22:23:42 -0300

    feat: criado as classes de request e responde de customers

--- modulo-spring/src/main/java/io/github/wesleyosantos91/api/v1/request/CustomerRequest.java
+++ modulo-spring/src/main/java/io/github/wesleyosantos91/api/v1/request/CustomerRequest.java
@@ -1,10 +1,19 @@
 package io.github.wesleyosantos91.api.v1.request;
 
-import java.time.Instant;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.github.wesleyosantos91.core.validation.Groups;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record CustomerRequest(git 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record CustomerRequest(
+        @NotBlank(groups = Groups.Create.class)
+        @Size(min = 2, max = 120, groups = Groups.Create.class)
         String name,
-        String email,
-        Instant createdAt
+        @NotBlank(groups = Groups.Create.class)
+        @Email(groups = Groups.Create.class)
+        String email
 ) {
 }
